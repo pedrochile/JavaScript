0ec94fd907a96855cc53f9bbb9f691f36a0d467c
pedrochile <pfernandezinfo@gmail.com>
2022-10-01 17:24:12 -0400

    update

--- final/app.js
+++ final/app.js
@@ -1,26 +1,27 @@
 class Productos {
-    constructor(id, nombre, marca, descripcion, img, cantidad, ) {
+    constructor(id, nombre, marca, descripcion, img, cantidad,precio ) {
       this.id = id
       this.nombre = nombre
       this.marca = marca
       this.descripcion= descripcion
       this.img=img
       this.cantidad = cantidad
+      this.precio =precio 
     }
   }
   const LocalS = window.localStorage;
   const stockProductos = [];
   
-  const prod2 = new Productos ("2", "alcohol Litro","Difem","Alcohol de Litro",'img/alcoholLitro.jpg', 30)
-  const prod1 = new Productos ("1", "Bolsa de basura 80x70", "Virutex", "bolsa de basura biodegradable", "img/bolsa70x90.jpg" , 50)
-  const prod3 = new Productos ("3", "Escobillon", "Virutex","Escobillon Basico","img/escobillon.jpg", 60)
-  const prod4 = new Productos ("4", "Papel Interfoliado ","Elite","Toallas Interfoliadas", `img/Interfoliada.jpg`, 80)
-  const prod5 = new Productos ("5", "Limpiador 4L","Virutex","Limpiador de pisos 4L", `img/limpiador4L.jpg`,56)
-  const prod6 = new Productos ("7", "Mascarillas","China Transfer","Mascarillas 3 pliegues",`img/mascarillas.jpg`,180)
-  const prod7 = new Productos ("8", "Mister Musculo","Musculin","Limpiador superficies",`img/musculo.jpg`,80)
-  const prod8 = new Productos ("9", "Papel Higienico","Tork","papel Higienico 4 x 500",`img/papelHigienico.jpg`,70)
-  const prod9 = new Productos ("10", "Papel Toalla","Ovella","Papel Absorvente",`img/papelToalla.jpg`,78)
-  const prod10 = new Productos ("11", "Trapero Doble","Virutex","trapero Doble Ojal",`img/trapero.jpg`,26)
+  const prod2 = new Productos ("1", "alcohol Litro","Difem","Alcohol de Litro",'img/alcoholLitro.jpg', 10,500)
+  const prod1 = new Productos ("2", "Bolsa de basura 80x70", "Virutex", "bolsa de basura biodegradable", "img/bolsa70x90.jpg" , 1,1000)
+  const prod3 = new Productos ("3", "Escobillon", "Virutex","Escobillon Basico","img/escobillon.jpg", 1,1500)
+  const prod4 = new Productos ("4", "Papel Interfoliado ","Elite","Toallas Interfoliadas", `img/Interfoliada.jpg`, 1,600)
+  const prod5 = new Productos ("5", "Limpiador 4L","Virutex","Limpiador de pisos 4L", `img/limpiador4L.jpg`,1,800)
+  const prod6 = new Productos ("7", "Mascarillas","China Transfer","Mascarillas 3 pliegues",`img/mascarillas.jpg`,1,700)
+  const prod7 = new Productos ("8", "Mister Musculo","Musculin","Limpiador superficies",`img/musculo.jpg`,1,800)
+  const prod8 = new Productos ("9", "Papel Higienico","Tork","papel Higienico 4 x 500",`img/papelHigienico.jpg`,1,400)
+  const prod9 = new Productos ("10", "Papel Toalla","Ovella","Papel Absorvente",`img/papelToalla.jpg`,1,900)
+  const prod10 = new Productos ("11", "Trapero Doble","Virutex","trapero Doble Ojal",`img/trapero.jpg`,1,450)
 
  
   
@@ -51,7 +52,7 @@ class Productos {
           button.addEventListener('click', () =>{
               cart(`${producto.id}`);
               alert(`Agregaste ${producto.nombre}`)
-              console.log(`button${producto.id}`)
+              
          
              
 
