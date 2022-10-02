89f515392e22c50533846e462934b3a015d6224c
pedrochile <pfernandezinfo@gmail.com>
2022-10-02 13:54:25 -0400

    update

--- final/app.js
+++ final/app.js
@@ -1,43 +1,133 @@
 class Productos {
-    constructor(id, nombre, marca, descripcion, img, cantidad,precio ) {
-      this.id = id
-      this.nombre = nombre
-      this.marca = marca
-      this.descripcion= descripcion
-      this.img=img
-      this.cantidad = cantidad
-      this.precio =precio 
-    }
+  constructor(id, nombre, marca, descripcion, img, cantidad, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.marca = marca;
+    this.descripcion = descripcion;
+    this.img = img;
+    this.cantidad = cantidad;
+    this.precio = precio;
   }
-  const LocalS = window.localStorage;
-  const stockProductos = [];
-  
-  const prod2 = new Productos ("1", "alcohol Litro","Difem","Alcohol de Litro",'img/alcoholLitro.jpg', 10,500)
-  const prod1 = new Productos ("2", "Bolsa de basura 80x70", "Virutex", "bolsa de basura biodegradable", "img/bolsa70x90.jpg" , 1,1000)
-  const prod3 = new Productos ("3", "Escobillon", "Virutex","Escobillon Basico","img/escobillon.jpg", 1,1500)
-  const prod4 = new Productos ("4", "Papel Interfoliado ","Elite","Toallas Interfoliadas", `img/Interfoliada.jpg`, 1,600)
-  const prod5 = new Productos ("5", "Limpiador 4L","Virutex","Limpiador de pisos 4L", `img/limpiador4L.jpg`,1,800)
-  const prod6 = new Productos ("7", "Mascarillas","China Transfer","Mascarillas 3 pliegues",`img/mascarillas.jpg`,1,700)
-  const prod7 = new Productos ("8", "Mister Musculo","Musculin","Limpiador superficies",`img/musculo.jpg`,1,800)
-  const prod8 = new Productos ("9", "Papel Higienico","Tork","papel Higienico 4 x 500",`img/papelHigienico.jpg`,1,400)
-  const prod9 = new Productos ("10", "Papel Toalla","Ovella","Papel Absorvente",`img/papelToalla.jpg`,1,900)
-  const prod10 = new Productos ("11", "Trapero Doble","Virutex","trapero Doble Ojal",`img/trapero.jpg`,1,450)
+}
 
- 
-  
-  stockProductos.push (prod1,prod2, prod3, prod4, prod5, prod6,prod7,prod8,prod9,prod10)
-  
-  //console.log(stockProductos)
-  
-  //crear una funcion para mostrar los prodcutos
+const stockProductos = [];
 
-  const mostrarProductos = (productos) =>   {
-      const productContainer = document.getElementById("contenedor-productos");
-      console.log("dsadas")
-      //console.log(productContainer)
-      productos.forEach( producto => {
-          const card = document.createElement('card');
-          card.innerHTML += `<div class="card border-danger mb-3" style="width:12rem;">
+const prod2 = new Productos(
+  "1",
+  "alcohol Litro",
+  "Difem",
+  "Alcohol de Litro",
+  "img/alcoholLitro.jpg",
+  10,
+  500
+);
+const prod1 = new Productos(
+  "2",
+  "Bolsa de basura 80x70",
+  "Virutex",
+  "bolsa de basura biodegradable",
+  "img/bolsa70x90.jpg",
+  1,
+  1000
+);
+const prod3 = new Productos(
+  "3",
+  "Escobillon",
+  "Virutex",
+  "Escobillon Basico",
+  "img/escobillon.jpg",
+  1,
+  1500
+);
+const prod4 = new Productos(
+  "4",
+  "Papel Interfoliado ",
+  "Elite",
+  "Toallas Interfoliadas",
+  `img/Interfoliada.jpg`,
+  1,
+  600
+);
+const prod5 = new Productos(
+  "5",
+  "Limpiador 4L",
+  "Virutex",
+  "Limpiador de pisos 4L",
+  `img/limpiador4L.jpg`,
+  1,
+  800
+);
+const prod6 = new Productos(
+  "7",
+  "Mascarillas",
+  "China Transfer",
+  "Mascarillas 3 pliegues",
+  `img/mascarillas.jpg`,
+  1,
+  700
+);
+const prod7 = new Productos(
+  "8",
+  "Mister Musculo",
+  "Musculin",
+  "Limpiador superficies",
+  `img/musculo.jpg`,
+  1,
+  800
+);
+const prod8 = new Productos(
+  "9",
+  "Papel Higienico",
+  "Tork",
+  "papel Higienico 4 x 500",
+  `img/papelHigienico.jpg`,
+  1,
+  400
+);
+const prod9 = new Productos(
+  "10",
+  "Papel Toalla",
+  "Ovella",
+  "Papel Absorvente",
+  `img/papelToalla.jpg`,
+  1,
+  900
+);
+const prod10 = new Productos(
+  "11",
+  "Trapero Doble",
+  "Virutex",
+  "trapero Doble Ojal",
+  `img/trapero.jpg`,
+  1,
+  450
+);
+
+stockProductos.push(
+  prod1,
+  prod2,
+  prod3,
+  prod4,
+  prod5,
+  prod6,
+  prod7,
+  prod8,
+  prod9,
+  prod10
+);
+
+localStorage.setItem("StockProductos", JSON.stringify(stockProductos));
+
+let ArrayLocalStorage = JSON.parse(localStorage.getItem("StockProductos"))
+
+//crear una funcion para mostrar los prodcutos
+
+const mostrarProductos = (ArrayLocalStorage) => {
+  const productContainer = document.getElementById("contenedor-productos");
+
+  ArrayLocalStorage.forEach((producto) => {
+    const card = document.createElement("card");
+    card.innerHTML += `<div class="card border-danger mb-3" style="width:12rem;">
                               <img src="${producto.img}" class="card-img-top" alt="...">
                                 <div class="card-body">
                                       <h5 class="card-title">${producto.nombre}</h5>
@@ -46,26 +136,14 @@ class Productos {
                                       <p id="quantity" class="card-text"> Cantidad: ${producto.cantidad}</p>
                                       <button class="btn btn-primary" id="button${producto.id}">Agregar al carro</button>
                                   </div>
-                              </div>`
-          productContainer.appendChild(card);
-          const button = document.getElementById(`button${producto.id}`);
-          button.addEventListener('click', () =>{
-              cart(`${producto.id}`);
-              alert(`Agregaste ${producto.nombre}`)
-              
-         
-             
+                              </div>`;
+    productContainer.appendChild(card);
+    const button = document.getElementById(`button${producto.id}`);
+    button.addEventListener("click", () => {
+      cart(`${producto.id}`);
+      alert(`Agregaste ${producto.nombre}`);
+    });
+  });
+};
 
-              
-
-
-
-          })
-      })
-  };
-  
-
-
-
-
- mostrarProductos(stockProductos)
+mostrarProductos(ArrayLocalStorage);
--- final/carrito.js
+++ final/carrito.js
@@ -1,74 +1,26 @@
 const cartContainter = document.querySelector(`#listado`);
 
-const productosCarro = [];
+let productosCarro = [];
+let CarroLocal = [];
 
-const cart = (productoId) => {
-  
 
+let cart = (productoId) => {
   const MostrarProductosENCarrito = () => {
+    cartContainter.textContent = "";
     //esto funciona
-    /* let productos = stockProductos.find((producto) => producto.id === productoId);
-    productosCarro.push(productos);
- */
-    /* function renderizarCarrito() {
-  // Vaciamos todo el html
-  DOMcarrito.textContent = '';
-  // Quitamos los duplicados
-  const carritoSinDuplicados = [...new Set(carrito)];
-  // Generamos los Nodos a partir de carrito
-  carritoSinDuplicados.forEach((item) => {
-      // Obtenemos el item que necesitamos de la variable base de datos
-      const miItem = stockProductos.filter((itemBaseDatos) => {
-          // ¿Coincide las id? Solo puede existir un caso
-          return itemBaseDatos.id === parseInt(item);
-      });
-      // Cuenta el número de veces que se repite el producto
-      const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-          // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
-          return itemId === item ? total += 1 : total;
-      }, 0); */
 
-    // esto no funciona
-    
-    let productos = stockProductos.find((producto) => 
-      producto.id === productoId);   
-   
+    let productos = ArrayLocalStorage.find(
+      (producto) => producto.id === productoId
+    );
 
     productosCarro.push(productos);
-    //LocalStorage.setItem('InfoCarro', JSON.stringify(productosCarro));
-    //cartContainter.textContent = ``
 
-    ///oscar preguntar
+    localStorage.setItem("productosCarro", JSON.stringify(productosCarro));
 
-    const carroFuncional = [...new Set(productosCarro)];
-    console.log(carroFuncional);
-   
-    carroFuncional.forEach((item) => {
-      console.log(item);
+    CarroLocal = JSON.parse(localStorage.getItem("productosCarro"));
+    localStorage.setItem("productosCarro", JSON.stringify([]));
 
-      const MiItem = stockProductos.filter((ItemProductos) =>{
-        return ItemProductos.id===parseInt(item)
-
-      })
-
-      const Unidades = productosCarro.reduce((cantidad,id)=>{
-        return id===item ? cantidad+=1: cantidad},0);
-
-      
-
-
-
-
-
-
-
-/*     const buscar = productosCarro.filter((itemcarrito) => {
-        return itemcarrito === parseInt(item);
-      }); */
-      /*   const ItemUnidades = productosCarro.reduce((cantidad, productoId) => {
-        return productoId === item ? (cantidad += 1) : cantidad;
-      }, 0);
- */
+    CarroLocal.forEach((item) => {
       let lista = document.createElement(`div`);
       lista.classList.add("row");
       //lista.setAttribute("id", productoId)
@@ -92,31 +44,27 @@ const cart = (productoId) => {
       colEliminar.innerHTML = `<button class="btn btn-danger btn-sm" id="${item.id}">Eliminar</button>`;
       lista.appendChild(colEliminar);
 
-      console.log(lista.childNodes);
       cartContainter.appendChild(lista);
 
       colEliminar.addEventListener(`click`, deleteProducto);
     });
-  }
-    MostrarProductosENCarrito();
-  
+  };
+  MostrarProductosENCarrito();
 };
 
 function deleteProducto(e) {
-  console.log(e.target);
-  ProductoELiminar = productosCarro.indexOf((elementoDelArray) => {
+  console.log(CarroLocal);
+  ProductoELiminar = CarroLocal.indexOf((elementoDelArray) => {
     elementoDelArray.id === e.target.id;
   });
 
-  productosCarro.splice(ProductoELiminar, 1);
-
-  console.log(e.target.offsetParent);
-  console.log(e.target.parentElement);
-  //  console.log(productosCarro)
+  CarroLocal.splice(ProductoELiminar, 1);
 
   //PREGUNTAR A OSCAR, POR QUE DA
   let btnClicked = e.target.parentElement.parentElement;
-  console.log(btnClicked);
   btnClicked.remove();
+
+  localStorage.setItem("productosCarro", JSON.stringify(CarroLocal));
+
   MostrarProductosENCarrito();
 }
